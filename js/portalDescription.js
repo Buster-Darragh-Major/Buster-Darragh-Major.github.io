79ca5a81417b82017fc379f784bc23dbd6df9432
Buster-Darragh-Major <bmaj406@aucklanduni.ac.nz>
2018-03-07 10:49:26 +1300

    made description dynamically appear

---
+++ js/portalDescription.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+	$("#work-portal").hover(
+		function() {
+			$("#portal-description").append("A brief work history inculding film production, civil engineering site work and a software engineering internship.");
+		},
+		function() {
+			$("#portal-description").empty();
+		}
+	);
+	
+	$("#education-portal").hover(
+		function() {
+			$("#portal-description").append("This area is coming soon, stay tuned!");
+		},
+		function(){
+			$("#portal-description").empty();
+		}
+	);
+
+	$("#music-portal").hover(
+		function() {
+			$("#portal-description").append("I love a lot of different kinds of music, and I try to listen to as much variety as I can! I've written a bit about some of my favourites.");
+		},
+		function(){
+			$("#portal-description").empty();
+		}
+	);
+
+	$("#simpsons-portal").hover(
+		function() {
+			$("#portal-description").append("From cheeky Mr. Burns to awfully devilish Miss Hoover, The Simpsons truly has it all!");
+			console.log("hey");
+		},
+		function(){
+			$("#portal-description").empty();
+		}
+	);
+})
